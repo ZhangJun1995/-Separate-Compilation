79881ba5b658e8c6d5f3daee68fccf27c3c752e2
ZhangJun <zjsdu2013@gmail.com>
2018-04-03 22:00:16 +0800

    Add files via upload

---
+++ ProjectVS/head.h
@@ -0,0 +1,7 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+int f(int val);
+int f(void);
+
+#endif
---
+++ ProjectVS/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "head.h" 
+using namespace std;
+
+int main()
+{
+	f(5);
+	f();
+
+	system("pause");
+	return 0;
+}
+
+
